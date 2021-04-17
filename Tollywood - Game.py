037287cc05b88d4443b4cc45ef1bf08b542871b9
Saroj Pedada <77237839+Saroj-Pedada@users.noreply.github.com>
2021-04-17 15:06:04 +0530

    Add files via upload

---
+++ Tollywood - Game.py
@@ -0,0 +1,97 @@
+import random
+import re
+import movlist
+
+
+while True :
+    print("\n\n\t\t\t\"A\" is for adding any new movies.")
+    print("\t\t\t\"P\" is for playing the game.")
+    user_input = input("\n\t\t\tEnter any character (P/A) : ")
+    if str(user_input) == "P" or str(user_input) == "p" :
+        ord_name = []
+        mov_name = []
+        print("\n\n\t\t\tLets Play TOLLYWOOD!\n\n")
+        mov_selected = random.choice(movlist.Movies)
+        unilen = len(set(mov_selected))
+        length = len(mov_selected)
+        for m in range(length) :
+            ord_name.append(ord(str(mov_selected[m])))
+        ord_name = sorted(ord_name)
+        ord_name = list(dict.fromkeys(ord_name))
+        print("The number of letters in the name of the movie are (may/mayn't include space) "+str(length)+".")
+        count_of_loss = 0
+        test_str = mov_selected
+        test_sub = " "
+        res = [i.start() for i in re.finditer(test_sub, test_str)]
+        numoftimes = len(res)
+        tin = []
+        for f in range(numoftimes) :
+            tin.append(res[f]+1)
+        if numoftimes == 0 :
+            print(length*" _ ")
+            print("\n\n\t\tCopy the above layout and start guessing. Good Luck!\n\n")
+        if numoftimes == 1 :
+            print((tin[0]-1)*" _ "+" | "+(length-tin[0])*" _ ")
+            print("\n\n\t\tCopy the above layout and start guessing. Good Luck!\n\n")
+        if numoftimes == 2 :
+            print((tin[0]-1)*" _ "+" | "+(tin[1]-tin[0]-1)*" _ "+" | "+(length-tin[1])*" _ ")
+            print("\n\n\t\tCopy the above layout and start guessing. Good Luck!\n\n")
+        if numoftimes == 3 :
+            print((tin[0]-1)*" _ "+" | "+(tin[1]-tin[0]-1)*" _ "+" | "+(tin[2]-tin[1]-1)*" _ "+" | "+(length-tin[2])*" _ ")
+            print("\n\n\t\tCopy the above layout and start guessing. Good Luck!\n\n")
+        for x in range(0,9+unilen-numoftimes) :
+            mov_name = sorted(mov_name)
+            a = input("Enter any letter : ")
+            if count_of_loss == 9 and ord_name != mov_name:
+                print("\n\nYou Lose! Better Luck Next Time.")
+                print("The name of the movie is "+mov_selected+".")
+                break
+            elif count_of_loss < 9 and mov_name == ord_name :
+                yn = input("You have exceeded the maximum number of trials. If you got the movie name type Y(YES) or N(NO) : ")
+                if str(yn) == "Y" or str(yn) == "y" :
+                    ans = input("\n\t\tEnter the movie name : ")
+                    if str(ans) == str(mov_selected) :
+                        print("\n\n\t\tYAY ! You won the game.")
+                    else :
+                        print("\n\n\t\tYou Lose! Better Luck Next Time.")
+                        print("\t\tThe name of the movie is "+mov_selected+".")
+                elif str(yn) == "N" or str(yn) == "n" :
+                    print("\n\n\t\tYou Lose! Better Luck Next Time.")
+                    print("\t\tThe name of the movie is "+mov_selected+".")
+                else :
+                    print("\n\n\t\tERROR 504! Please restart The program.")
+            else :
+                for y in range(length) :
+                    if str(a) == str(mov_selected[y]) :
+                        print("Correct Guess! It is in position number "+str(y+1)+".")
+                        mov_name.append(ord(str(a)))
+                        mov_name = list(dict.fromkeys(mov_name))
+                if str(a) not in str(mov_selected) :
+                        count_of_loss+=1
+                        print("Not present in the title. You lost a chance. You still have "+str(9-count_of_loss+1)+" chances left.")
+    elif str(user_input) == "A" or str(user_input) == "a" :
+        while True :
+            nameofmov = input("\n\t\tEnter the name of the movie to add : ")
+            f = open("movlist.py", "a")
+            f.write("\nMovies.append(\""+nameofmov+"\")\nMovies = list(dict.fromkeys(Movies))")
+            f.close()
+            print("\n\n\t\t\tThank You For Adding")
+            ny = input("\n\t\tIf you want to add more , press Y(YES) or N(NO) : ")
+            if str(ny) == "Y" or str(ny) == "y" :
+                continue
+            elif str(ny) == "N" or str(ny) == "n" :
+                break
+            else :
+                print("\n\n\t\tERROR 504! Please restart the program")
+                break
+    else :
+        print("\n\n\t\tERROR 504! Please restart the program")
+    yon = input("\n\n\tPress Y(YES) to restart the program or N(NO) to stop : ")
+    if str(yon) == "Y" or str(yon) == "y" :
+        continue
+    elif str(yon) == "N" or str(yon) == "n" :
+        print("\n\n\t\tThank You For Playing !")
+        break
+    else :
+        print("\n\n\t\tERROR 504! Please restart the program")
+        break
---
+++ movlist.py
@@ -0,0 +1,34 @@
+Movies = ["ninnu kori","brochevarevarura","disco raja","naayak","mr majnu","sarrainodu","master","aakasam nee haddura","devudu chesina manushulu",
+"krishnarjuna yuddham","geetha govindam","arjun suravaram","krack","raja the great","bahubali","saaho","legend","jaanu","oh baby","nenu local","red","f2",
+"guru","oopiri","rangasthalam","majnu","naandi","arjun reddy","world famous lover","liger","naannaku prematho","jai lava kusa","radhe shyam","adipurush","gentleman",
+"jersey","ishq","tuck jagadeesh","dear comrade","shyam singha roy","ashta chamma","sarileru neekevvaru","bheeshma","rangde","mahanati","kandireega",
+"ala vaikuntapuramlo","gang leader","uppena","jathi ratnalu","life is beautiful","fidaa","mukunda","tholi prema","entha manchivadavura","aswathama",
+"prathi roju pandage","solo brathuke so better","agent sai srinivas athreya","vakeel saab","zombie reddy","mister","robo","kanchana","bhagamathi",
+"raju gari gadhi","pilla jamindar","parugu","duvvada jagannatham","julai","penguin","rx 100","sye raa","dhruva","most eligible bachelor","love story",
+"colour photo","taxiwaala","hushaaru","bruce lee","padi padi leche manasu","sashi","chaavu kaburu challaga","naa peru surya","orange","aravinda sametha",
+"yemaaya chesave","savaari","murder","aacharya""maari","middle class abbayi","khaidi no 150","nota","bharath ane nenu","athadu","chirutha","singham",
+"chalo","chal mohan ranga","raahu","sreekaram","maharshi","hello","akhil","manmadhudu","kushi","agnathavasi","pelli choopulu","palasa","hit","awe","u turn",
+"majili","vinaya vidheya rama","unnadi okate zindagi"]
+Movies.append("mayabazar")
+Movies.append("mathu vadalara")
+Movies = list(dict.fromkeys(Movies))
+Movies.append("venky mama")
+Movies = list(dict.fromkeys(Movies))
+Movies.append("ghaazi")
+Movies = list(dict.fromkeys(Movies))
+Movies.append("atharintiki daredi")
+Movies = list(dict.fromkeys(Movies))
+Movies.append("o pitta kadha")
+Movies = list(dict.fromkeys(Movies))
+Movies.append("jenda pai kapi raju")
+Movies = list(dict.fromkeys(Movies))
+Movies.append("aagadu")
+Movies = list(dict.fromkeys(Movies))
+Movies.append("seethamma vakitlo sirimalle chettu")
+Movies = list(dict.fromkeys(Movies))
+Movies.append("srimanthudu")
+Movies = list(dict.fromkeys(Movies))
+Movies.append("dookudu")
+Movies = list(dict.fromkeys(Movies))
+Movies.append("kalyana vaibhogame")
+Movies = list(dict.fromkeys(Movies))
